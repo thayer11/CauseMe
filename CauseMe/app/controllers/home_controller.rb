395108c32a81c2a271db18b3535223b43abb1634
thayer11 <holly.coddington@gmail.com>
2016-11-21 14:06:23 -0700

    about route & controller

---
+++ CauseMe/app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+ class HomeController < ApplicationController
+
+    ## This is a controller action called about_us
+    def about_us
+      render text: "About the project"
+    end
+
+  end
---
+++ CauseMe/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+   get '/about_us', to: 'home#about_us'
+end
